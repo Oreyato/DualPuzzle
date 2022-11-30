6425fd9940df4f124495765fc93f6e3d9678ca71
oreyato <oreyato.pro@gmail.com>
2022-11-30 18:03:27 +0100

    game scene

--- DualPuzzle/src/game/Scene_DualPuzzle.cpp
+++ DualPuzzle/src/game/Scene_DualPuzzle.cpp
@@ -22,6 +22,18 @@ void Scene_DualPuzzle::setGame(Game *_game)
 void Scene_DualPuzzle::load()
 {
     std::srand((int) std::time(nullptr));
+
+	// Load all textures
+
+	// Load level from level index
+
+
+}
+
+void Scene_DualPuzzle::loadLevel(int i) {
+	// Browse levels json file and get the right one
+
+
 }
 
 void Scene_DualPuzzle::clean()
@@ -45,6 +57,10 @@ void Scene_DualPuzzle::update(float dt)
 }
 
 void Scene_DualPuzzle::draw() {
+	// Draw background
+	// glDrawArraysInstanced(GL_RECTANGLE, 0, 6, 1024 * 1024);
+
+	
 }
 
 int Scene_DualPuzzle::getRand(int a, int b)
--- DualPuzzle/src/game/Scene_DualPuzzle.h
+++ DualPuzzle/src/game/Scene_DualPuzzle.h
@@ -19,12 +19,37 @@ public:
 	void draw();
 	void setGame(Game *);
 
-
+	void loadLevel(int i);
 
 private:
 	Game *game;
 
 	int getRand(int a, int b);
+
+	//v Level init values ============================================
+	// Level
+	int levelWidth;
+	int levelHeight;
+	// Tile bgTiles; <- pas besoin, seront dessinées au début du draw()
+
+	// Players
+	// Player players
+	// Player A
+	int plAStartXPos;
+	int plAStartYPos;
+	// Player B
+	int plBStartXPos;
+	int plBStartYPos;
+
+	// End tiles
+	// Endtile endTiles
+	// End A
+	int plAEndXPos;
+	int plAEndYPos;
+	// End B
+	int plBEndXPos;
+	int plBEndYPos;
+	//^ Level init values ============================================
 };
 
 #endif // Scene_DualPuzzle_H
