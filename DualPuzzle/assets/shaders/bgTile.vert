28b24b860155e790c52c1299266491329c4dd23a
oreyato <oreyato.pro@gmail.com>
2022-12-06 17:10:46 +0100

    feat: draw background

--- DualPuzzle/assets/shaders/bgTile.vert
+++ DualPuzzle/assets/shaders/bgTile.vert
@@ -1,19 +1,21 @@
 #version 410 core
 
+layout (location = 0) in vec2 offset;
+
 out vec4 vertColor;
 
 void main()
 {
-    const vec4 backgroundTileVerticies[3] = vec4[3] (
+    const vec4 backgroundTileVerticies[6] = vec4[6] (
 		vec4(-0.05f,  0.05f, 0.0f, 1.0f),
 		vec4( 0.05f, -0.05f, 0.0f, 1.0f),
-		vec4(-0.05f, -0.05f, 0.0f, 1.0f)//,
+		vec4(-0.05f, -0.05f, 0.0f, 1.0f),
 
-		// vec4(-0.05f,  0.05f, 0.0f, 1.0f), 
-     	// vec4( 0.05f, -0.05f, 0.0f, 1.0f), 
-     	// vec4( 0.05f,  0.05f, 0.0f, 1.0f)	
+		vec4(-0.05f,  0.05f, 0.0f, 1.0f), 
+     	vec4( 0.05f, -0.05f, 0.0f, 1.0f), 
+     	vec4( 0.05f,  0.05f, 0.0f, 1.0f)	
     );
 
-    gl_Position = backgroundTileVerticies[gl_VertexID];
-    vertColor = vec4(0.0, 1.0, 0.0, 1.0);
+    gl_Position = backgroundTileVerticies[gl_VertexID] + vec4(offset, 0.0, 0.0);
+    vertColor = vec4(0.35, 0.35, 0.35, 1.0);
 } 
--- DualPuzzle/src/game/Scene_DualPuzzle.cpp
+++ DualPuzzle/src/game/Scene_DualPuzzle.cpp
@@ -25,38 +25,37 @@ void Scene_DualPuzzle::load()
     std::srand((int) std::time(nullptr));
 
 	Assets::loadShader("assets/shaders/bgTile.vert", "assets/shaders/bgTile.frag", "", "", "", "bgTile");
-	// Assets::loadShader(SHADER_VERT(SHADER_NAME), SHADER_FRAG(SHADER_NAME), "", "", SHADER_GEOM(SHADER_NAME), SHADER_ID(SHADER_NAME));
 
 	shader = Assets::getShader("bgTile");
 
     glCreateVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-	// // Prepare offsets
-	// Vector2 translations[100];
-	// int index = 0;
-	// float offset = 0.1f;
-	// for(int y = -10; y < 10; y += 2) {
-	// 	for(int x = -10; x < 10; x += 2) {
-	// 		Vector2 translation;
-	// 		translation.x = (float)x / 10.0f + offset;
-	// 		translation.y = (float)y / 10.0f + offset;
-	// 		translations[index++] = translation;
-	// 	}
-	// }  
+	// Prepare offsets
+	Vector2 translations[100];
+	int index = 0;
+	float offset = 0.1f;
+	for(int y = -10; y < 10; y += 2) {
+		for(int x = -10; x < 10; x += 2) {
+			Vector2 translation;
+			translation.x = (float)x / 10.0f + offset;
+			translation.y = (float)y / 10.0f + offset;
+			translations[index++] = translation;
+		}
+	}  
 
-	// unsigned int instanceVBO;
-	// glGenBuffers(1, &instanceVBO);
-	// glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-	// glBufferData(GL_ARRAY_BUFFER, sizeof(Vector2)*100, &translations[0], GL_STATIC_DRAW);
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
+	unsigned int instanceVBO;
+	glGenBuffers(1, &instanceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vector2)*100, &translations[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	// glEnableVertexAttribArray(0);
-	// glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-	// glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);	
-	// glVertexAttribDivisor(0, 1); // This function tells OpenGL when to update the 
-	// 						 // content of a vertex attribute to the next element
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);	
+	glVertexAttribDivisor(0, 1); // This function tells OpenGL when to update the 
+							 	 // content of a vertex attribute to the next element
 
 	// Load all textures
 
@@ -92,17 +91,17 @@ void Scene_DualPuzzle::update(float dt)
 }
 
 void Scene_DualPuzzle::drawBackground() {
-	static const GLfloat bgColor[] = {0.0f, 0.0f, 0.0f, 1.0f};
-    glClearBufferfv(GL_COLOR, 0, bgColor);
-
 	shader.use();
 	glBindVertexArray(vao);
-	// glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 1);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100);
+	// glDrawArrays(GL_TRIANGLES, 0, 3);
 	glBindVertexArray(0);
 }
 
 void Scene_DualPuzzle::draw() {
+	static const GLfloat bgColor[] = {0.75f, 0.75f, 0.75f, 1.0f};
+    glClearBufferfv(GL_COLOR, 0, bgColor);
+
 	drawBackground();
 }
 
